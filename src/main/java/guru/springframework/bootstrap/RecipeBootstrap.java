c842d7e6faa9aebdf51430915b12ff8ee9d76bbd
OvidiuMariciuc <ovidiu_mariciuc17@yahoo.com>
2022-01-07 11:34:09 +0200

    test commit

--- src/main/java/guru/springframework/bootstrap/RecipeBootstrap.java
+++ src/main/java/guru/springframework/bootstrap/RecipeBootstrap.java
@@ -39,7 +39,7 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
         loadCategories();
         loadUom();
         recipeRepository.saveAll(getRecipes());
-        log.debug("Loading Bootstrap Data");
+        log.debug("Loading Bootstrap Data...");
     }
 
     private void loadCategories(){
